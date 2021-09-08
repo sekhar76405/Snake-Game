095389ee292ce365e3afab17e61f2857e45597c6
sekhar76405 <ssjio5402@gmail.com>
2021-09-08 16:05:10 +0530

    implemented checkApple(), if apple is eaten or not

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -91,7 +91,11 @@ public class GamePanel extends JPanel implements ActionListener{
 	
 	}
 	public void checkApple() {
-		
+		if((x[0] == appleX) && (y[0] == appleY)) {
+			applesEaten++;
+			bodyParts++;
+			newApple();
+		}
 	}
 	public void checkCollisions() {
 		// checks if head collides with body
