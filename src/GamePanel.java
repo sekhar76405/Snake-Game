ee47d8ea447605c656b27d905f81809c7bbea0f7
sekhar76405 <ssjio5402@gmail.com>
2021-09-08 16:17:16 +0530

    removed the grid view

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -45,10 +45,11 @@ public class GamePanel extends JPanel implements ActionListener{
 	public void draw(Graphics g) {
 		
 		if(running) {
+			/*
 			for(int i=0;i<SCREEN_HEIGHT/UNIT_SIZE;i++) {
 				g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
 				g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
-			}
+			}*/
 			g.setColor(Color.red);
 			g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);  //drawing apple in the grid with random (x,y)
 			
