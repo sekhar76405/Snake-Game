c58f4d79fcc2abd45d2fb53135907060613cf3ed
sekhar76405 <ssjio5402@gmail.com>
2021-09-08 14:29:30 +0530

    Made 3 files for Frame, Panel & main

---
+++ src/GamePanel.java
@@ -0,0 +1,17 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements ActionListener{
+
+	GamePanel(){
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
