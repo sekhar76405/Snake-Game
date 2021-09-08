50a54b95d88180e4a4df5be45912403f6ea69a04
sekhar76405 <ssjio5402@gmail.com>
2021-09-08 15:03:49 +0530

    Declared basic funcc that we need in GamePanel

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -9,5 +9,7 @@ public class GameFrame extends JFrame{
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
 		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
 	}
 }
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,5 +1,7 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
 
 import javax.swing.JPanel;
 
@@ -9,9 +11,38 @@ public class GamePanel extends JPanel implements ActionListener{
 		
 	}
 	
+	public void startGame() {
+		
+	}
+	public void paintComponent(Graphics g) {
+		
+	}
+	public void draw(Graphics g) {
+		
+	}
+	public void move() {
+		
+	}
+	public void checkApple() {
+		
+	}
+	public void checkCollisions() {
+		
+	}
+	public void gameOver(Graphics g) {
+		
+	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		
+		
+	}
+	
+	public class MyKeyAdapter extends KeyAdapter{
+		@Override
+		public void keyPressed(KeyEvent e) {
+			
+		}
 	}
 }
