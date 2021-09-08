174d0b4e977b9f4b8a86caef12054fdc277edd53
sekhar76405 <ssjio5402@gmail.com>
2021-09-08 15:33:03 +0530

    defined draw() & created a grid of the display

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -3,8 +3,9 @@ public class SnakeGame {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi");
-
+		//System.out.println("Hi");
+		GameFrame g =  new GameFrame();
+		GamePanel p =  new GamePanel();
 	}
 
 }
