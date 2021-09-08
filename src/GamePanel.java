f280e4f06e60b37414687f95621908bc4085c350
sekhar76405 <ssjio5402@gmail.com>
2021-09-08 15:15:39 +0530

    Declared & initialized all the required data members of the GamePanel

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -7,8 +7,29 @@ import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements ActionListener{
 
+	static final int SCREEN_WIDTH =600;
+	static final int SCREEN_HEIGHT =600;
+	static final int UNIT_SIZE = 25;
+	static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT)/UNIT_SIZE;
+	static final int DELAY = 75;
+	final int x[] = new int[GAME_UNITS];  //will store the X coordinates of Snake
+	final int y[] = new int[GAME_UNITS];  //will store the Y coordinates of Snake
+	int bodyParts = 6;
+	int applesEaten =0;
+	int appleX;		//will store the X coordinates of Apple
+	int appleY;		//will store the Y coordinates of Apple
+	char direction = 'R'; 		//assuming as R,L,U,D and hardcoding that Snake will move to R when game starts 
+	boolean running = false;
+	Timer timer;
+	Random random;
+	
 	GamePanel(){
-		
+		random = new Random();
+		this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
+		this.setBackground(Color.black);
+		this.setFocusable(true);
+		this.addKeyListener(new MyKeyAdapter());
+		startGame();
 	}
 	
 	public void startGame() {
@@ -18,6 +39,9 @@ public class GamePanel extends JPanel implements ActionListener{
 		
 	}
 	public void draw(Graphics g) {
+		
+	}
+	public void newApple() {
 		
 	}
 	public void move() {
